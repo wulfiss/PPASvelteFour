3a33df53bd73dcfd70e1c95e278c0d8876fd267f
Julian <96155980+wulfiss@users.noreply.github.com>
2024-06-24 10:00:36 +0000

    css changes for better fitting in the screens

--- src/routes/chlorine/+page.svelte
+++ src/routes/chlorine/+page.svelte
@@ -43,13 +43,16 @@
   }
 
 </script>
-  
-<div class="overflow-x-auto">
+<div id="header" class="flex flex-row justify-center gap-2">
   <label class="input input-bordered flex items-center gap-2">
     <input type="text" class="grow" placeholder="Filtrar por fecha, sector o grifo" bind:value={searchTerm}/>
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4 opacity-70"><path fill-rule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clip-rule="evenodd" /></svg>
   </label>
-    <table class="table table-zebra">
+  <button class="btn" on:click={handleClick}><svg width="24" height="24" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M11.75 3a.75.75 0 0 1 .743.648l.007.102.001 7.25h7.253a.75.75 0 0 1 .102 1.493l-.102.007h-7.253l.002 7.25a.75.75 0 0 1-1.493.101l-.007-.102-.002-7.249H3.752a.75.75 0 0 1-.102-1.493L3.752 11h7.25L11 3.75a.75.75 0 0 1 .75-.75Z" fill="#212121"/></svg></button>
+  <Downloadbtn />
+</div>
+<div class="overflow-x-auto">
+  <table class="table table-zebra">
       <!-- head -->
       <thead>
         <tr>
@@ -57,7 +60,7 @@
           <th class="text-center">Hora</th>
           <th class="text-center">Sector</th>
           <th class="text-center">Grifo</th>
-          <th class="text-center">Concentración (ppm)</th>
+          <th class="text-center">Concentración<br>(ppm)</th>
         </tr>
       </thead>
       <tbody>
@@ -74,13 +77,16 @@
       </tbody>
     </table>
   </div>
-  {#if filteredItems.length > 0}
-  <PaginationBtn totalItems={filteredItems.length} 
-  {itemsPerPage} 
-  {currentPage} 
-  on:pageChange={handlePageChange}/>
-  {:else}
-  <p class="text-center">No hay datos</p>
-  {/if}
-  <button class="btn fixed bottom-4 right-40" on:click={handleClick}>Agregar medición</button>
-  <Downloadbtn />
+  <div class="flex flex-row justify-center mt-1">
+    {#if filteredItems.length > 0}
+    <PaginationBtn totalItems={filteredItems.length} 
+    {itemsPerPage} 
+    {currentPage} 
+    on:pageChange={handlePageChange}/>
+    {:else}
+    <p class="text-center">No hay datos</p>
+    {/if}
+  </div>
+
+  
+  
