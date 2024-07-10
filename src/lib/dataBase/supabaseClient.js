e0ad78737083393a5b1f29996b18821243e4609c
Julian <96155980+wulfiss@users.noreply.github.com>
2024-07-09 22:34:23 -0300

    chore: Update Supabase client to use environment variables in supabaseClient.js
    The code changes update the Supabase client in `supabaseClient.js` to use the environment variables `VITE_SUPABASE_URL` and `VITE_SUPABASE_ANON_KEY` instead of the `process.env` variables. This ensures that the Supabase client is configured with the correct credentials for authentication and database access.
    
    This commit message follows the established convention of using a prefix (`chore`) to indicate a chore or maintenance task and provides a clear and concise description of the code changes.

--- src/lib/dataBase/supabaseClient.js
+++ src/lib/dataBase/supabaseClient.js
@@ -2,7 +2,7 @@ import { VITE_SUPABASE_ANON_KEY, VITE_SUPABASE_URL } from '$env/static/private';
 import { createBrowserClient } from '@supabase/ssr';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_ANON_KEY;
+const supabaseUrl = VITE_SUPABASE_URL;
+const supabaseKey = VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createBrowserClient(supabaseUrl || '', supabaseKey || '');
