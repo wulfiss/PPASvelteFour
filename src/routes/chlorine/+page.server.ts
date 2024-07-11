1341800a20dbc0f042d31b851257f67c86a77fce
wulfiss <96155980+wulfiss@users.noreply.github.com>
2024-07-11 05:14:18 -0300

    refactor: Update Supabase client to order by 'fecha' and 'hora' in chlorine page

--- src/routes/chlorine/+page.server.ts
+++ src/routes/chlorine/+page.server.ts
@@ -2,29 +2,6 @@ import { error, fail, redirect } from '@sveltejs/kit'; // Import SvelteKit's err
 import { supabase } from '$lib/dataBase/supabaseClient.js';
 import type { Actions, PageServerLoad } from './$types';
 
-/* export const actions: Actions = {
-  deleteItem: async ({ request }) => {
-    const formData = await request.formData();
-    const id = formData.get('id');
-
-    if (!id) {
-      return fail(400, {message: 'Missing ID'});
-    }
-
-    const { error: supabaseError } = await supabase
-      .from('cloroLibre')
-      .delete()
-      .eq('id', id);
-
-    if (supabaseError) {
-      console.error('Error deleting data from Supabase:', supabaseError.message);
-      return fail(500, {message: 'Error deleting data from Supabase'});
-    }
-
-    throw redirect(303, '/chlorine');
-  }
-} */
-
 export const load: PageServerLoad = async ({ depends }) => {
 	depends('cloroLibre');
 
