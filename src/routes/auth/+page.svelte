48116be80b9cf742534c14ea89b876cc5406e361
Julian <96155980+wulfiss@users.noreply.github.com>
2024-07-14 00:03:28 -0300

    feat: Add authentication layout and page for login and signup
    This commit adds a new layout component `auth/+layout.svelte` and a new page component `auth/+page.svelte` for handling authentication-related functionality. The layout component includes a navigation bar with a link to the home page, and the page component includes a form for user login and signup. These additions enhance the user experience by providing a consistent layout and dedicated authentication functionality.

---
+++ src/routes/auth/+page.svelte
@@ -0,0 +1,12 @@
+<form action="?/login" method="post">
+    <label>
+        Email
+        <input type="email" name="email" id="">
+    </label>
+    <label>
+        Password
+        <input type="password" name="password" id="">
+    </label>
+    <button>Login</button>
+    <button formaction="?/signup">Sign up</button>
+</form>
