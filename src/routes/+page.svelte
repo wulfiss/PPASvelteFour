ed18e26d416707b6f5d881d162663847c927c604
Julian <96155980+wulfiss@users.noreply.github.com>
2024-07-14 00:03:16 -0300

    refactor: Update supabaseClient import in supabaseClient.js

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,8 +6,13 @@
         goto('/chlorine');
     }
 
+    function handleClickLogIn() {
+        goto('/auth');
+    }
+
 </script>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <button class="btn btn-active btn-primary" on:click={handleClick}>Cloro</button>
+<button class="btn btn-active btn-primary" on:click={handleClickLogIn}>Log in</button>
