c9372eba1de4235017f143f7d4c66a5ca5c28fe2
Julian <96155980+wulfiss@users.noreply.github.com>
2024-07-14 00:03:55 -0300

    feat: Add authentication confirmation route
    This commit adds a new route `auth/confirm` with the corresponding server-side code in `auth/confirm/+server.ts`. The route handles the confirmation of email OTP tokens and redirects the user to the specified `next` URL upon successful verification. If the verification fails, the user is redirected to the error page. This addition enhances the authentication flow and improves the user experience.

---
+++ src/routes/private/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { invalidate } from '$app/navigation';
+	import type { EventHandler } from 'svelte/elements';
+
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+	$: ({ notes, supabase, user } = data);
+</script>
+
+<h1>Private page for user: {user?.email}</h1>
