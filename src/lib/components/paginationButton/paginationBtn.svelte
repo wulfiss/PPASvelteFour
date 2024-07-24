f16ef5fb11980a5b31f5ade2e990b1a8c2585876
Julian <96155980+wulfiss@users.noreply.github.com>
2024-07-24 07:46:22 +0000

    fix styling buttons

--- src/lib/components/paginationButton/paginationBtn.svelte
+++ src/lib/components/paginationButton/paginationBtn.svelte
@@ -1,40 +1,41 @@
 <script lang="ts">
-    import { createEventDispatcher } from "svelte";
+	import { createEventDispatcher } from 'svelte';
 
-    export let totalItems: number = 0;
-    export let itemsPerPage: number = 10;
-    export let currentPage: number = 1;
+	export let totalItems: number = 0;
+	export let itemsPerPage: number = 10;
+	export let currentPage: number = 1;
 
-    const dispatch = createEventDispatcher<{pageChange: {page: number}}>();
+	const dispatch = createEventDispatcher<{ pageChange: { page: number } }>();
 
-    $: totalPages = Math.ceil(totalItems / itemsPerPage);
-    $: visiblePages = getVisiblePages(currentPage, totalPages);
+	$: totalPages = Math.ceil(totalItems / itemsPerPage);
+	$: visiblePages = getVisiblePages(currentPage, totalPages);
 
-    function getVisiblePages(currentPage: number, total: number):(number | string)[] {
-        if(total <= 7) return [...Array(total)].map((_, i) => i + 1);
-        
-        if(currentPage < 5) return [1, 2, 3, 4, 5, '...', total];
-        if(currentPage > total - 4) return [1, '...', total - 4, total - 3, total - 2, total - 1, total];
-    
-        return [1, '...', currentPage - 1, currentPage, currentPage + 1, '...', total];
-    }
+	function getVisiblePages(currentPage: number, total: number): (number | string)[] {
+		if (total <= 7) return [...Array(total)].map((_, i) => i + 1);
 
-    function changePage(page: string | number): void {
-        if (typeof page === 'number' && page !== currentPage){
-            currentPage = page;
-            dispatch('pageChange', { page });
-        }
-    }
+		if (currentPage < 5) return [1, 2, 3, 4, 5, '...', total];
+		if (currentPage > total - 4)
+			return [1, '...', total - 4, total - 3, total - 2, total - 1, total];
+
+		return [1, '...', currentPage - 1, currentPage, currentPage + 1, '...', total];
+	}
+
+	function changePage(page: string | number): void {
+		if (typeof page === 'number' && page !== currentPage) {
+			currentPage = page;
+			dispatch('pageChange', { page });
+		}
+	}
 </script>
 
-<div class="btn-grup">
-    {#each visiblePages as page}
-        <button
-            class="btn btn-sm {page === currentPage ? 'btn-active' : ''}"
-            on:click={() => changePage(page)}
-            disabled={page === '...'}
-        >
-            {page}
-        </button>
-    {/each}
+<div class="join">
+	{#each visiblePages as page}
+		<button
+			class="join-item btn {page === currentPage ? 'btn-active' : ''}"
+			on:click={() => changePage(page)}
+			disabled={page === '...'}
+		>
+			{page}
+		</button>
+	{/each}
 </div>
