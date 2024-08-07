c8260d3eb24bf1e259eb9a69b692ad5faa1509d8
Julian <96155980+wulfiss@users.noreply.github.com>
2024-08-06 22:21:59 -0300

    refactor: Update eviscerado page forms layout and remove unused code

---
+++ src/routes/private/eviscerado/formSectorOne/BirdSizes.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import type { sectorOneData } from "$lib/types";
+    import type { Writable } from "svelte/store";
+
+    export let sectorOneStore: Writable<sectorOneData>;
+</script>
+
+<div class="card bg-base-100 rounded-box h-fit grid flex-grow place-items-center">    
+    <div class="divider divider-primary w-3/4">
+        Tamaños de aves
+    </div>
+    {#each ['chicos', 'medianos', 'grandes', 'desparejos'] as size}
+        <div class="form-control w-3/4 md:w-full">
+            <label class="label cursor-pointer">
+                <span class="label-text">{size.charAt(0).toUpperCase() + size.slice(1)}</span>
+                <input
+                    type="checkbox"
+                    bind:checked={$sectorOneStore[`pollos_${size}`]}
+                    class="checkbox checkbox-primary"
+                />
+            </label>
+        </div>      
+    {/each}
+</div>
