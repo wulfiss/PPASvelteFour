54674addd499cae7e19b36edf6e69f6115973caf
Julian <96155980+wulfiss@users.noreply.github.com>
2024-08-06 22:21:46 -0300

    refactor: Update eviscerado page forms layout and remove unused code

--- src/routes/private/eviscerado/modal/forms/Allform.svelte
+++ src/routes/private/eviscerado/modal/forms/Allform.svelte
@@ -1,4 +1,6 @@
-<script lang="ts">
+<!-- <script lang="ts">
+	import sectorOneStore from "$lib/stores/sectorOneStore";
+
 	let date = new Date();
 	let today = date.toISOString().slice(0, 10); // YYYY-MM-DD
 	let time = date.toLocaleTimeString('en-US', {
@@ -7,84 +9,6 @@
 		minute: '2-digit'
 	});
 
-	let data = {
-		fecha: today,
-		productor: '',
-		pollos_chicos: false,
-		pollos_medianos: false,
-		pollos_grandes: false,
-		pollos_desparejos: false,
-		buches: 0,
-		buches_chicos: false,
-		buches_medianos: false,
-		buches_grandes: false,
-		pollos_rotos: 0,
-		alas_dislocadas_hematomas_recientes: 0,
-		alas_dislocadas_hematomas_viejos: 0,
-		alas_dislocadas_sin_hematomas: 0,
-		alas_fracturadas_hematomas_recientes: 0,
-		alas_fracturadas_hematomas_viejos: 0,
-		alas_fracturadas_sin_hematomas: 0,
-		alas_puntas_fracturadas: 0,
-		miopatias: 0,
-		dermatitis: 0,
-		dermatitis_leve: false,
-		dermatitis_moderada: false,
-		dermatitis_severa: false,
-		traqueas: 0,
-		intermedia_observaciones: '',
-		garras_lindas: false,
-		garras_regulares: false,
-		garras_feas: false,
-		garras_observaciones: '',
-		cogotes: 0,
-		pulmones: 0,
-		cloacas: 0,
-		patas_fracturadas: 0,
-		sangre: false,
-		eviscerado_observaciones: ''
-	};
-
-	function resetForm() {
-		data = {
-			fecha: today,
-			productor: '',
-			pollos_chicos: false,
-			pollos_medianos: false,
-			pollos_grandes: false,
-			pollos_desparejos: false,
-			buches: 0,
-			buches_chicos: false,
-			buches_medianos: false,
-			buches_grandes: false,
-			pollos_rotos: 0,
-			alas_dislocadas_hematomas_recientes: 0,
-			alas_dislocadas_hematomas_viejos: 0,
-			alas_dislocadas_sin_hematomas: 0,
-			alas_fracturadas_hematomas_recientes: 0,
-			alas_fracturadas_hematomas_viejos: 0,
-			alas_fracturadas_sin_hematomas: 0,
-			alas_puntas_fracturadas: 0,
-			miopatias: 0,
-			dermatitis: 0,
-			dermatitis_leve: false,
-			dermatitis_moderada: false,
-			dermatitis_severa: false,
-			traqueas: 0,
-			intermedia_observaciones: '',
-			garras_lindas: false,
-			garras_regulares: false,
-			garras_feas: false,
-			garras_observaciones: '',
-			cogotes: 0,
-			pulmones: 0,
-			cloacas: 0,
-			patas_fracturadas: 0,
-			sangre: false,
-			eviscerado_observaciones: ''
-		};
-	}
-
 	async function onFormSubmit() {
 		try {
 			const response = await fetch('/private/eviscerado/endpoints/allForm', {
@@ -92,13 +16,12 @@
 				headers: {
 					'Content-Type': 'application/json'
 				},
-				body: JSON.stringify(data)
+				body: JSON.stringify($sectorOneStore)
 			});
 			if (!response.ok) {
 				throw new Error('Error al enviar los datos');
 			}
-
-			resetForm();
+			sectorOneStore.set({});
 		} catch (error) {
 			console.error('Error:', error);
 			alert('Error al enviar los datos');
@@ -113,35 +36,6 @@
 	on:submit|preventDefault={onFormSubmit}
 >
 
-	<div class="card bg-base-100 rounded-box h-fit grid flex-grow place-items-center">
-			<label class="form-control w-3/4 md:w-full">
-				<div class="label">
-					<span class="label-text">Fecha</span>
-				</div>
-				<input
-					type="date"
-					name="date"
-					bind:value={data.fecha}
-					placeholder="Type here"
-					class="input input-bordered"
-				/>
-				<div class="label"></div>
-			</label>
-	
-			<label class="form-control w-3/4 md:w-full">
-				<div class="label">
-					<span class="label-text">Productor</span>
-				</div>
-				<input
-					type="text"
-					name="granja"
-					bind:value={data.productor}
-					placeholder="nombre"
-					class="input input-bordered"
-				/>
-				<div class="label"></div>
-			</label>
-		</div>
 
 	<div class="divider lg:divider-horizontal"></div>
 
@@ -609,3 +503,4 @@
 		{/if}
 	</div>
 </form>
+ -->
