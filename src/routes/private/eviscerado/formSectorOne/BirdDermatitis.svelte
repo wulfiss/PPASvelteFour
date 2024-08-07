91af06b65ba1a8308be2286a4be5fa3d1cfe5a32
Julian <96155980+wulfiss@users.noreply.github.com>
2024-08-07 09:50:18 +0000

    feat: Add BirdDermatitis component for eviscerado formSectorOne

---
+++ src/routes/private/eviscerado/formSectorOne/BirdDermatitis.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import type { sectorOneData } from '$lib/types';
+	import type { Writable } from 'svelte/store';
+
+	export let sectorOneStore: Writable<sectorOneData>;
+</script>
+
+<label class="form-control w-3/4 md:w-full">
+	<div class="label">
+		<span class="label-text">Dermatitis (%)</span>
+	</div>
+	<input
+		bind:value={$sectorOneStore.dermatitis}
+		type="number"
+		name="puntasFracturadas"
+		min="0"
+		placeholder="Type here"
+		class="input input-bordered"
+	/>
+	<div class="label"></div>
+</label>
+
+{#each ['leve', 'moderada', 'severa'] as grade}
+	<div class="form-control w-3/4 md:w-full">
+		<label class="label cursor-pointer">
+			<span class="label-text"> Dermatitis {grade}</span>
+			<input
+				type="checkbox"
+				bind:checked={$sectorOneStore[`dermatitis_${grade}`]}
+				class="checkbox checkbox-primary"
+			/>
+		</label>
+	</div>
+{/each}
