74078f11841d8290c1c75006634141152075deec
Julian <96155980+wulfiss@users.noreply.github.com>
2024-08-11 03:20:07 -0300

    feat: Add debounce function for search input in processReport page

--- src/routes/private/processReport/+page.svelte
+++ src/routes/private/processReport/+page.svelte
@@ -8,8 +8,6 @@
 		goto(`/private/processReport/${fecha}/${productor}`);
 	};
 
-
-
 	$: ({ Sector_One, totalItems, pageSize, searchTerm, page } = data);
 	$: totalPages = Math.ceil(totalItems / pageSize);
 
@@ -32,6 +30,13 @@
 		goto(`?page=1`);
 	}
 
+	const debounce = (callback: Function, wait = 200) => {
+    	let timeout: ReturnType<typeof setTimeout>;
+    	return (...args: any[]) => {
+        	clearTimeout(timeout);
+        	timeout = setTimeout(() => callback(...args), wait);
+    	};
+	};
 
 </script>
 
@@ -42,14 +47,27 @@
 			class="grow"
 			placeholder="Filtrar por fecha o productor"
 			bind:value={searchInput}
-			on:keyup={(e) => e.key === 'Enter' && handleSearch()}
+			on:keyup={debounce(handleSearch)}
 		/>
 	</label>
 	<button class="btn" on:click={handleSearch}>
 		<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
 	</button>
 	{#if searchTerm}
-		<button class="btn btn-sm mb-4" on:click={clearSearch}>Limpiar búsqueda</button>
+		<button class="btn btn-square" on:click={clearSearch}>
+			<svg
+			xmlns="http://www.w3.org/2000/svg"
+			class="h-6 w-6"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke="currentColor">
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-width="2"
+				d="M6 18L18 6M6 6l12 12" />
+			</svg>
+	  	</button>
 	{/if}
 </div>
 
@@ -76,10 +94,14 @@
 	  </table>
 </div>
 
-<div class="btn-group flex flex-row justify-center gap-2 mt-3">
-	<button class="btn" disabled={page === 1} on:click={() => changePage(page - 1)}>«</button>
+<div class="join flex flex-row justify-center mt-3">
+	<button class="join-item btn" disabled={page === 1} on:click={() => changePage(page - 1)}>«</button>
 	{#each Array(totalPages) as _, i}
-		<button class="btn" class:btn-active={page === i + 1} on:click={() => changePage(i + 1)}>{i + 1}</button>
+	  {#if i + 1 === 1 || i + 1 === totalPages || (i + 1 >= page - 1 && i + 1 <= page + 1)}
+		<button class="join-item btn" class:btn-active={page === i + 1} on:click={() => changePage(i + 1)}>{i + 1}</button>
+	  {:else if (i === 1 || i === totalPages - 2) && (page > 3 && page < totalPages - 2)}
+		<button class="join-item btn btn-disabled">...</button>
+	  {/if}
 	{/each}
-	<button class="btn" disabled={page === totalPages} on:click={() => changePage(page + 1)}>»</button>
+	<button class="join-item btn" disabled={page === totalPages} on:click={() => changePage(page + 1)}>»</button>
 </div>
