2fc877cacd842ae2e9de603d675a56266a96b1da
Julian <96155980+wulfiss@users.noreply.github.com>
2024-08-11 03:19:59 -0300

    refactor: Update page server load function for Chlorine page

--- src/routes/private/chlorine/+page.svelte
+++ src/routes/private/chlorine/+page.svelte
@@ -1,47 +1,11 @@
 <script lang="ts">
 	import Downloadbtn from './downloadButton/downloadBtn.svelte';
 	import PaginationBtn from '$lib/components/paginationButton/paginationBtn.svelte';
-	import { invalidate } from '$app/navigation';
+	import { goto, invalidate } from '$app/navigation';
 	import DeleteConfirmationModal from './modal/deleteModal/DeleteModal.svelte';
 	import AddFormModal from './modal/addFormModal/AddFormModal.svelte';
-	import { page } from '$app/stores';
-
-	/* export let data; */
-	$: data = $page.data;
-
-	/* let filteredItems: any[]; */
-	let searchTerm = '';
-
-	$: filteredItems = data.cloroLibre.filter(
-		(item: { fecha: string; sector: string; grifo: { toString: () => string } }) => {
-			const searchLower = searchTerm.toLowerCase();
-
-			return (
-				item.fecha.toLowerCase().includes(searchLower) ||
-				item.sector.toLowerCase().includes(searchLower) ||
-				item.grifo.toString().toLowerCase().includes(searchLower)
-			);
-		}
-	);
-
-	let currentPage = 1;
-	let itemsPerPage = 10;
-
-	$: totalPages = Math.ceil(filteredItems.length / itemsPerPage);
-
-	$: paginatedItems = filteredItems.slice(
-		(currentPage - 1) * itemsPerPage,
-		currentPage * itemsPerPage
-	);
-
-	$: {
-		searchTerm;
-		currentPage = 1;
-	}
-
-	function handlePageChange(event: CustomEvent<{ page: number }>) {
-		currentPage = event.detail.page;
-	}
+    import { page as currentPage } from '$app/stores';
+	import type { PageData } from './$types';
 
 	type ModalState = {
 		isFormOpen: boolean;
@@ -79,6 +43,39 @@
 			return 'badge badge-lg badge-error gap-2';
 		}
 	};
+
+	export let data: PageData;
+	
+	$: ({ cloroLibre, totalItems, pageSize, searchTerm, page } = data);
+	
+	$: totalPages = Math.ceil(totalItems / pageSize);
+
+	let searchInput = searchTerm;
+
+	function handleSearch() {
+		if (searchInput.trim() === '') {
+			goto(`?page=1`);
+		} else {
+			goto(`?search=${encodeURIComponent(searchInput)}&page=1`);
+		}
+	}
+
+	function changePage(newPage: number) {
+		goto(`?search=${encodeURIComponent(searchTerm)}&page=${newPage}`);
+	}
+
+	function clearSearch() {
+		searchInput = '';
+		goto(`?page=1`);
+	}
+
+	const debounce = (callback: Function, wait = 200) => {
+    	let timeout: ReturnType<typeof setTimeout>;
+    	return (...args: any[]) => {
+        	clearTimeout(timeout);
+        	timeout = setTimeout(() => callback(...args), wait);
+    	};
+	};
 </script>
 
 <div id="header" class="flex flex-row justify-center gap-2">
@@ -87,19 +84,19 @@
 			type="text"
 			class="grow"
 			placeholder="Filtrar por fecha, sector o grifo"
-			bind:value={searchTerm}
+			bind:value={searchInput}
+			on:keyup={debounce(handleSearch)}
 		/>
 		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			viewBox="0 0 16 16"
-			fill="currentColor"
-			class="w-4 h-4 opacity-70"
-			><path
-				fill-rule="evenodd"
-				d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-				clip-rule="evenodd"
-			/></svg
-		>
+		xmlns="http://www.w3.org/2000/svg"
+		viewBox="0 0 16 16"
+		fill="currentColor"
+		class="h-4 w-4 opacity-70">
+		<path
+		  fill-rule="evenodd"
+		  d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+		  clip-rule="evenodd" />
+	  </svg>
 	</label>
 	<button class="btn" on:click={openFormModal}
 		><svg width="24" height="24" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
@@ -125,9 +122,9 @@
 		</thead>
 		<tbody>
 			<!-- body -->
-			{#each paginatedItems as item, i}
+			{#each cloroLibre as item, i}
 				<tr>
-					<th class="text-center">{item.fecha}</th>
+					<th class="text-center font-normal">{item.fecha}</th>
 					<td class="text-center">{item.hora}</td>
 					<td class="text-center">{item.sector}</td>
 					<td class="text-center">{item.grifo}</td>
@@ -139,7 +136,6 @@
 					<td class="text-center flex">
 						<!-- <button class="btn btn-sm btn-primary mr-2">Editar</button> -->
 						<button on:click={() => openDeleteModal(item)} class="btn btn-ghost btn-xs">
-							
 							Eliminar
 						</button>
 						<!-- <DeleteButton id={item.id} on:deleted={handleItemDeleted} /> -->
@@ -149,18 +145,17 @@
 		</tbody>
 	</table>
 </div>
-<div class="flex flex-row justify-center mt-1">
-	{#if filteredItems.length > 0}
-		<PaginationBtn
-			totalItems={filteredItems.length}
-			{itemsPerPage}
-			{currentPage}
-			on:pageChange={handlePageChange}
-		/>
-	{:else}
-		<p class="text-center">No hay datos</p>
-	{/if}
-</div>
+<div class="join flex flex-row justify-center mt-3">
+	<button class="join-item btn" disabled={page === 1} on:click={() => changePage(page - 1)}>«</button>
+	{#each Array(totalPages) as _, i}
+	  {#if i + 1 === 1 || i + 1 === totalPages || (i + 1 >= page - 1 && i + 1 <= page + 1)}
+		<button class="join-item btn" class:btn-active={page === i + 1} on:click={() => changePage(i + 1)}>{i + 1}</button>
+	  {:else if (i === 1 || i === totalPages - 2) && (page > 3 && page < totalPages - 2)}
+		<button class="join-item btn btn-disabled">...</button>
+	  {/if}
+	{/each}
+	<button class="join-item btn" disabled={page === totalPages} on:click={() => changePage(page + 1)}>»</button>
+  </div>
 
 <DeleteConfirmationModal
 	bind:item={modalState.itemToDelete}
