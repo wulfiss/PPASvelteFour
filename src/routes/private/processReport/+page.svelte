57f82d3e68ad456688c30e986bccd778ffba63ee
Julian <96155980+wulfiss@users.noreply.github.com>
2024-08-13 20:52:12 -0300

    refactor: Improve search functionality in private/+page.svelte

--- src/routes/private/processReport/+page.svelte
+++ src/routes/private/processReport/+page.svelte
@@ -1,40 +1,29 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 	import type { PageData } from './$types';
+
+	import Pagination from '$lib/components/pagination/Pagination.svelte';
+	import handleSearch from '$lib/utils/searchBarHandler';
+
+	import debounce from '$lib/utils/debounce';
+
 	export let data: PageData;
 
-	const navigateToDetails = (fecha: never, productor: never) => {
-		goto(`/private/processReport/${fecha}/${productor}`);
-	};
-
 	$: ({ Sector_One, totalItems, pageSize, searchTerm, page } = data);
-	$: totalPages = Math.ceil(totalItems / pageSize);
 
 	let searchInput = searchTerm;
 
-	function handleSearch() {
-		if (searchInput.trim() === '') {
-			goto(`?page=1`);
-		} else {
-			goto(`?search=${encodeURIComponent(searchInput)}&page=1`);
-		}
-	}
-
-	function changePage(newPage: number) {
-		goto(`?search=${encodeURIComponent(searchTerm)}&page=${newPage}`);
-	}
+	const search = debounce(() => {
+		handleSearch(searchInput);
+	}, 400);
 
 	function clearSearch() {
 		searchInput = '';
 		goto(`?page=1`);
 	}
 
-	const debounce = (callback: Function, wait = 350) => {
-    	let timeout: ReturnType<typeof setTimeout>;
-    	return (...args: any[]) => {
-        	clearTimeout(timeout);
-        	timeout = setTimeout(() => callback(...args), wait);
-    	};
+	const navigateToDetails = (fecha: never, productor: never) => {
+		goto(`/private/processReport/${fecha}/${productor}`);
 	};
 
 </script>
@@ -46,10 +35,10 @@
 			class="grow"
 			placeholder="Filtrar por fecha o productor"
 			bind:value={searchInput}
-			on:keyup={debounce(handleSearch)}
+			on:keyup={search}
 		/>
 	</label>
-	<button class="btn" on:click={handleSearch}>
+	<button class="btn" on:click={search}>
 		<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
 	</button>
 	{#if searchTerm}
@@ -93,14 +82,9 @@
 	  </table>
 </div>
 
-<div class="join flex flex-row justify-center mt-3">
-	<button class="join-item btn" disabled={page === 1} on:click={() => changePage(page - 1)}>«</button>
-	{#each Array(totalPages) as _, i}
-	  {#if i + 1 === 1 || i + 1 === totalPages || (i + 1 >= page - 1 && i + 1 <= page + 1)}
-		<button class="join-item btn" class:btn-active={page === i + 1} on:click={() => changePage(i + 1)}>{i + 1}</button>
-	  {:else if (i === 1 || i === totalPages - 2) && (page > 3 && page < totalPages - 2)}
-		<button class="join-item btn btn-disabled">...</button>
-	  {/if}
-	{/each}
-	<button class="join-item btn" disabled={page === totalPages} on:click={() => changePage(page + 1)}>»</button>
-</div>
+<Pagination
+	searchTerm={searchInput}
+	page={page}
+	pageSize={pageSize}
+	totalItems={totalItems}
+/>
