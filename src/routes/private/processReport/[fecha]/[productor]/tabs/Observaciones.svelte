096cd893cb3e5ceb7186c20ba0faa949d9b08ddf
Julian <96155980+wulfiss@users.noreply.github.com>
2024-08-26 21:52:47 -0300

    Update Observaciones.svelte to display general observations data

--- src/routes/private/processReport/[fecha]/[productor]/tabs/Observaciones.svelte
+++ src/routes/private/processReport/[fecha]/[productor]/tabs/Observaciones.svelte
@@ -1,1 +1,23 @@
-<h1>UnderConstruction</h1>
+<script lang="ts">
+    
+    export let data;
+
+</script>
+<div class="overflow-x-auto">
+    <table class="table table-zebra">
+      <thead>
+			<tr>
+				<th class="text-center">Fecha</th>
+				<th class="text-center">Observaciones</th>
+			</tr>
+      </thead>
+      <tbody>
+        {#each data as Observaciones, i}
+        <tr>
+            <th class="text-center">{Observaciones.fecha}</th>
+            <td class="text-center w-3/4">{Observaciones.observaciones_generales}</td>
+        </tr>
+        {/each}
+      </tbody>
+    </table>
+  </div>
