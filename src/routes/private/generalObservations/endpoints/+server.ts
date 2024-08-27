900202c73547121751f3cefe24dcdd6eed610552
Julian <96155980+wulfiss@users.noreply.github.com>
2024-08-26 21:11:03 -0300

    feat: Add POST endpoint for inserting general observations
    This commit adds a new POST endpoint to the server file in the "generalObservations" endpoints directory. The endpoint allows authenticated users to insert general observations into the "General_Observations" table in the database. It validates the user's session, checks for authorization, and inserts the data into the table using the Supabase library. If there is an error, it throws an appropriate error response. This enhancement improves the functionality of the application by providing a way to store and retrieve general observations from the database.

---
+++ src/routes/private/generalObservations/endpoints/+server.ts
@@ -0,0 +1,28 @@
+import { json, error } from "@sveltejs/kit";
+import type { RequestHandler } from './$types';
+
+export const POST: RequestHandler = async ({ locals: { supabase, safeGetSession }, request }) => {
+       
+    const session = await safeGetSession();
+    
+    if (!session) {
+        throw error(401, 'Unauthorized');
+    }
+
+    try{
+        const data = await request.json();
+
+        const { error: dbError } = await supabase
+        .from("General_Observations")
+        .insert(data);
+
+        if (dbError) {
+            throw error(500, { message: `Supabase error: ${dbError.message}` });
+        }
+        return json({ message: "Data inserted successfully"});
+    } catch (err) {
+        console.error('Error processing request:', err);
+        throw error(500, 'Internal server error');
+    }
+   
+}
